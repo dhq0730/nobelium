15ad708d39398c691b7c7b14a6a8c145aa85a38b
dhq0730 <109465247+dhq0730@users.noreply.github.com>
2022-07-19 14:30:41 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,9 +1,9 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
+  title: 'quanlanguage',
+  author: 'Denghq',
   email: 'i@craigary.net',
-  link: 'https://nobelium.vercel.app',
-  description: 'This gonna be an awesome website.',
+  link: 'quanlanguage.me',
+  description: 'Share anything interesting about language.',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
