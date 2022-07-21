20b0a4d84471e7206e37c8d8da5672c354d21496
dhq0730 <109465247+dhq0730@users.noreply.github.com>
2022-07-21 22:29:21 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -5,9 +5,9 @@ const BLOG = {
   link: 'quanlanguage.me',
   description: 'Share anything interesting about language.',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
-  appearance: 'auto', // ['light', 'dark', 'auto'],
+  appearance: 'light', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
-  lightBackground: '#ffffff', // use hex value, don't forget '#' e.g #fffefc
+  lightBackground: '#fffefc', // use hex value, don't forget '#' e.g #fffefc
   darkBackground: '#18181B', // use hex value, don't forget '#'
   path: '', // leave this empty unless you want to deploy Nobelium in a folder
   since: 2021, // If leave this empty, current year will be used.
